20e1d2fccd0a0e1387013217d92b0f45bced8c17
Fernando Paz <ferpaz9292@gmail.com>
2019-04-23 23:26:13 -0300

    Ahora el ajuste de tau es lineal y se calcula la dimension fractal

--- problema1d.py
+++ problema1d.py
@@ -15,42 +15,28 @@ taus=np.zeros(probas)
 nat=np.zeros(len(datos[1,:]))
 
 #rango=len(datos[1,:])
-rango=1500
 
-
-ns=lambda tpl,x : -tpl[0]*x-np.log(sp.zeta(tpl[0]-1))+np.log(tpl[1])
-
-def ErrorFunc(tpl,x,y):
-    if(tpl[0]>2.04 and tpl[0]<3):
-        return ns(tpl,x)-y
-    else:
-        return 1e10*np.ones(len(x))
+rango_min=200
+rango=500
 
 
 
 
-x=np.log(range(2,rango))
+
+x=np.log(range(rango_min,rango))
 for i in range(0,probas):
-    y=np.log(datos[i,2:rango])
+    y=np.log(datos[i,rango_min:rango])
     p=np.polyfit(x,y,1)
-    #b=sp.zeta(2.05-1)*np.exp(p[1])
-    tplInitial1=(2.041, 100)
-    tplFinal1,success=leastsq(ErrorFunc,tplInitial1[:],args=(x,y),maxfev=1000)
-    taus[i]=tplFinal1[0]
-    chi_squared[i]=np.sum(ErrorFunc(tplFinal1,x,y)**2)
+    taus[i]=p[0]
+    chi_squared[i]=np.sum((np.polyval(p,x)-y)**2)
     
  
 plt.figure(1)
-x=np.log(range(2,rango))
-y=np.log(datos[1270,2:rango])
+x=np.log(range(rango_min,rango))
+y=np.log(datos[100,rango_min:rango])
 p=np.polyfit(x,y,1)
-tplInitial1=(2.04,10)
-tplFinal1,success=leastsq(ErrorFunc,tplInitial1[:],args=(x,y),maxfev=1000)
 line1 = plt.scatter(x,y,s=1)
-line2 = plt.plot(x,ns(tplFinal1,x))
 line2 = plt.plot(x,np.polyval(p,x))
-#plt.xscale("log")
-#plt.yscale("log")
 plt.show()
 
 plt.figure(2)
--- problema3.py
+++ problema3.py
@@ -17,17 +17,54 @@ datos=np.loadtxt('dim_fractal',dtype=float)
 
 
 l=datos[:,0]
-m=datos[:,2]
+m=datos[:,5]
 
 P=np.zeros(len(m))
 
-for i in range(0,len(l)):
+for i in range(1,len(l)):
 	P[i]=m[i]/(l[i]*l[i])		
 
-
+p=np.polyfit(np.log(l[2:30]),np.log(m[2:30]),1)
 
 plt.figure(1)
-plt.scatter(l,np.log10(P))
+plt.scatter(np.log(l[2:101]),np.log(m[2:101]),s=2)
+plt.plot(np.log(l[2:101]),np.polyval(p,np.log(l[2:101])))
 plt.show(block=True)
+
+D=91/48
+
+
+rango=30
+
+lab=''
+
+for i in range(2,6):
+    D=np.polyfit(np.log(l[2:rango]),np.log(datos[2:rango,i]),1)[0]
+    lab =lab + 'D = {}'.format(D)
+
+size=2
+
+plt.figure(1)
+line1 = plt.scatter(np.log(l[2:101]),np.log(datos[2:101,2]),s=size,label=lab[0:22])
+line1a= plt.plot(np.log(l[2:101]),np.polyval(np.polyfit(np.log(l[2:rango]),np.log(datos[2:rango,2]),1),np.log(l[2:101])))
+
+line2 = plt.scatter(np.log(l[2:101]),np.log(datos[2:101,3]),s=size,label=lab[22:44])
+line2a= plt.plot(np.log(l[2:101]),np.polyval(np.polyfit(np.log(l[2:rango]),np.log(datos[2:rango,3]),1),np.log(l[2:101])))
+
+line3 = plt.scatter(np.log(l[2:101]),np.log(datos[2:101,4]),s=size,label=lab[44:66])
+line3a= plt.plot(np.log(l[2:101]),np.polyval(np.polyfit(np.log(l[2:rango]),np.log(datos[2:rango,4]),1),np.log(l[2:101])))
+
+line4 = plt.scatter(np.log(l[2:101]),np.log(datos[2:101,5]),s=size,label=lab[66:88])
+line4a= plt.plot(np.log(l[2:101]),np.polyval(np.polyfit(np.log(l[2:rango]),np.log(datos[2:rango,5]),1),np.log(l[2:101])))
+
+plt.xlabel('Log(L)')
+plt.ylabel('Log(M)')
+#axes = plt.gca()
+#axes.set_xlim([0.55,0.63])
+#axes.set_ylim([0.55,0.65])
+legend1 = plt.legend(handles=[line1,line2,line3,line4], loc='Best')
+plt.show(block=True)
+
+
 
 
