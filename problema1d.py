10741bec8f092ea9a7c4f610f7af33d5476b2aed
Fernando Paz <ferpaz9292@gmail.com>
2019-04-29 05:55:59 -0300

    curvas colapsadas a f(z), calculo de sigma

--- problema1d.py
+++ problema1d.py
@@ -14,8 +14,8 @@ taus=np.zeros(probas)
 
 nat=np.zeros(len(datos[1,:]))
 
-rango_min=100
-rango=700
+rango_min=4
+rango=80
 #rango=len(datos[1,:])
 
 
@@ -32,29 +32,36 @@ for i in range(0,probas):
  
 plt.figure(1)
 x=np.log(range(rango_min,rango))
-y=np.log(datos[100,rango_min:rango])
+y=np.log(datos[2,rango_min:rango])
 p=np.polyfit(x,y,1)
-line1 = plt.scatter(x,y,s=size)
-line2 = plt.plot(x,np.polyval(p,x))
+line1 = plt.scatter(x,y,s=size,label='ns')
+line2, = plt.plot(x,np.polyval(p,x),label='Ajuste lineal')
+plt.xlabel('log(s)')
+plt.ylabel('log(ns)')
+legend1 = plt.legend(handles=[line1,line2], loc='Best')
 plt.show()
 
 plt.figure(2)
-plt.scatter(datos[:,0],chi_squared,s=size)
+line1 = plt.scatter(datos[:,0],chi_squared,s=size,label='$\\chi$')
+legend1 = plt.legend(handles=[line1], loc='Best')
+plt.xlabel('p')
+plt.ylabel('Error cuadratico')
 plt.show()
 
 
 
-plt.figure(3)
-plt.scatter(datos[:,0],taus,s=size)
-plt.show()
+
 
 np.min(chi_squared)
 indice_optimo=np.where(chi_squared == min(chi_squared))[0][0]
 pc_64=datos[indice_optimo,0]
-taus[indice_optimo]
+tau=taus[indice_optimo]
 
-#pc64=0.59256
-pc64=datos[indice_optimo,0]
+
+print('El valor de taus es:  ',tau)
+
+pc64=0.59256
+#pc64=datos[indice_optimo,0]
 
 
 pmax=np.zeros(len(datos[0,:])-1)
@@ -68,21 +75,35 @@ for i in range(1,len(pmax)):
 
 
 
-argum=pc64*np.ones(len(pmax))-pmax
+argum=(pmax-pc64*np.ones(len(pmax)))
 fin=len(argum)
-plt.figure(3)
-plt.scatter(natural[3:fin],argum[3:fin],s=size)
+x=(natural[1450:1700])
+y=(argum[1450:1700])
+plt.figure(4)
+plt.scatter(x,y,s=size)
+p2=np.polyfit(x,y,1)
+plt.plot(x,p2[0]*x+p2[1]*np.ones(len(x)))
 #plt.xscale('log')
 #plt.yscale('log')
 plt.show()
 
 
-plt.figure(4)
-line1 = plt.scatter(datos[:,0],datos[:,1],s=size,label='1')
-line2 = plt.scatter(datos[:,0],datos[:,2],s=size,label='2')
-##plt.scatter(datos[:,0],datos[:,100],s=size)
-plt.xscale('log')
-plt.yscale('log')
-legend1 = plt.legend(handles=[line1,line2], loc='Best')
+
+a=np.ones(len(datos[:,0]))
+
+b=0.242
+plt.figure(5)
+line3 = plt.scatter((datos[:,0]-pc64*a)*(3**b),datos[:,3]/datos[12,3],s=size,label='s=3')
+line2 = plt.scatter((datos[:,0]-pc64*a)*(2**b),datos[:,2]/datos[12,2],s=size,label='s=2')
+line4 = plt.scatter((datos[:,0]-pc64*a)*(4**b),datos[:,4]/datos[12,4],s=size,label='s=4')
+line5 = plt.scatter((datos[:,0]-pc64*a)*(5**b),datos[:,5]/datos[12,5],s=size,label='s=5')
+line6 = plt.scatter((datos[:,0]-pc64*a)*(6**b),datos[:,6]/datos[12,6],s=size,label='s=6')
+line7 = plt.scatter((datos[:,0]-pc64*a)*(50**b),datos[:,50]/datos[12,50],s=size,label='s=50')
+plt.xlabel('z')
+plt.ylabel('$\\frac{<ns(p)>}{<ns(p_c)>}$')
+legend1 = plt.legend(handles=[line2,line3,line4,line5,line6,line7], loc='Best')
 plt.show()
+
+
+print('El coeficiente $\\sigma$ es: ',b)
 
